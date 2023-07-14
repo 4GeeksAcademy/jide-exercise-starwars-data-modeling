b08630950965249a21a9b814b6859fdbd46edb00
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-07-14 18:48:30 +0000

    first try completed

--- src/models.py
+++ src/models.py
@@ -16,16 +16,37 @@ class User(Base):
     username = Column(String(250), nullable=False)
     password = Column(String(250), nullable=False)
     loginStatus = Column(String(250), nullable=False)
-    registerDate = Column
+    # registerDate = Column(Date)
 
 class Characters(Base):
     __tablename__ = 'characters'
     # Here we define columns for the table characters.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
+    name = Column(String(250))
+    gender = Column(String(250))
+    hairColor = Column(String(250))
+    eyeColor = Column(String(250))
     
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table planets.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    population = Column(Integer)
+    terrain = Column(String(250))
+    climate = Column(String(250))
+
+class Vehicles(Base):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table vehicles.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    model = Column(String(250))
+    manufacturer = Column(String(250))
+    crew = Column(Integer)
   
 
     def to_dict(self):
